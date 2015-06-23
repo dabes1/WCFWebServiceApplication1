8d427185756ecca024a3d53c47c7eafa71a121cd
Darrell Abes <dabes1@hotmail.com>
2015-06-23 11:14:39 -0700

    Initial Load for WCF Web Service Application

---
+++ WCFWebServiceApplication1/DataObjects/DataObject1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// Added usings
+using System.Runtime.Serialization;
+
+namespace WCFWebServiceApplication1.DataObjects
+{
+    
+    [DataContract]
+    public class DataObject1
+    {
+        private int _id;
+        private string _desc;
+        private List<int> _list;
+
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Desc { get; set; }
+
+
+        [DataMember]
+        public List<int> ListInt { get; set; }
+    }
+}
---
+++ WCFWebServiceApplication1/Service1.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+// Added usings
+using WCFWebServiceApplication1.DataObjects;
+
+namespace WCFWebServiceApplication1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(string value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public DataObject1 GetDataObject(string id)
+        {
+            DataObject1 _out = new DataObject1();
+
+            _out.Id = Convert.ToInt32(id);
+            _out.Desc = "DataObject Created for Id:" + id;
+
+            List<int> oList = new List<int>();
+
+            for (int i = 0; i < Convert.ToInt32(id); i++)
+                oList.Add(i);
+
+            _out.ListInt = oList;
+
+            return _out;
+        }
+    }
+}
